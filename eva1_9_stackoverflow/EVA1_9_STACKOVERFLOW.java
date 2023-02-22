8ef46632451137abe913517e87a89cf5a8c83316
EduardoMunozN <124702009+EduardoMunozN@users.noreply.github.com>
2023-02-22 09:48:11 -0600

    Add files via upload

---
+++ eva1_9_stackoverflow/EVA1_9_STACKOVERFLOW.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.eva1_9_stackoverflow;
+
+/**
+ *
+ * @author eduar
+ */
+public class EVA1_9_STACKOVERFLOW {
+
+    public static void main(String[] args) {
+        A();
+    }
+    public static void A(){
+        A();
+    }
+}
